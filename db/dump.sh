c00c1d342c42a484e76e5687f946a386e470d6bd
Nik <nik@khilnani.org>
2014-09-27 08:50:58 -0400

    Update dump.sh

--- db/dump.sh
+++ db/dump.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Dumping"
-pg_dump --host 127.0.0.1 --username USER --password DBNAME > DBNAME.sql
+pg_dump --host 127.0.0.1 --username postgres --password DBNAME > DBNAME.sql
 
 echo "Dumping clean"
-pg_dump --host 127.0.0.1 --username USER --password DBNAME --clean > DBNAME-clean.sql
+pg_dump --host 127.0.0.1 --username postgres --password DBNAME --clean > DBNAME-clean.sql
 
 echo "Dumping data"
-pg_dump --host 127.0.0.1 --username USER --password --data-only DBNAME > DBNAME-data.sql
+pg_dump --host 127.0.0.1 --username postgres --password --data-only DBNAME > DBNAME-data.sql
 
 echo "Dumping data-inserts"
-pg_dump --host 127.0.0.1 --username USER --password --data-only --column-inserts DBNAME > DBNAME-data-insert.sql
+pg_dump --host 127.0.0.1 --username postgres --password --data-only --column-inserts DBNAME > DBNAME-data-insert.sql
