22eba31a96a3098070336e98eeedf2ba73dcac50
khilnani <nik@khilnani.org>
2016-11-15 23:45:17 -0500

    updated

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'media/'))
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
