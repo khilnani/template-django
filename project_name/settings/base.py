aa3e5fe7e64227ea951fad2073ddc7354bf2a451
Nik <nik@khilnani.org>
2016-11-20 22:35:41 -0500

    Update base.py

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
